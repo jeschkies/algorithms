9859633a1b145a09bfecc29b646551917ef2c4eb
Karsten Jeschkies <k@jeschkies.xyz>
2020-04-14 15:19:42 +0200

    Add Cargo files.

---
+++ kahan/src/lib.rs
@@ -0,0 +1,15 @@
+
+/// Accurate floating point summation.
+///
+/// This uses [Kahan Algorithm](https://en.wikipedia.org/wiki/Kahan_summation_algorithm).
+pub fn sum(values: &[f32]) -> f32 {
+
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
