0d6ef23ab52088db4dee8f51ad9a407de8d551d5
Karsten Jeschkies <k@jeschkies.xyz>
2020-04-16 11:56:59 +0200

    First SIMD version

--- kahan/src/lib.rs
+++ kahan/src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(stdsimd)]
+use faster::*;
+
 /// Accurate floating point summation.
 ///
 /// This uses [Kahan Algorithm](https://en.wikipedia.org/wiki/Kahan_summation_algorithm).
@@ -19,7 +22,18 @@ pub fn sum(values: &[f64]) -> f64 {
 ///
 /// This implementations uses SIMD instruction to speed things up.
 pub fn sum_simd(values: &[f64]) -> f64 {
-    sum(values)
+    let mut c = f64s(0.0);
+
+    values.simd_iter(f64s(0.0))
+        .simd_reduce(f64s(0.0), |s, v| {
+            let y = v - c;
+            let t = s + y;
+            c = (t - s) - y;
+            let new_s = t;
+
+            new_s
+        })
+        .scalar_reduce(0.0, |acc, s| acc + s)
 }
 
 #[cfg(test)]
@@ -29,6 +43,12 @@ mod tests {
     #[test]
     fn it_works() {
         let result = sum(&vec![10000.0, 3.14159, 2.71828]);
+        assert_eq!(result, 10005.85987,);
+    }
+
+    #[test]
+    fn simd_works() {
+        let result = sum_simd(&vec![10000.0, 3.14159, 2.71828]);
         assert_eq!(result, 10005.85987,);
     }
 }
